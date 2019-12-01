3f462703d5a64130f354aff015389a22f6426c2c
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-12-01 17:46:35 +1300

    Updates

---
+++ precache-manifest.3632d393271c3be1c8e1d6552201734d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c88788dd0636c45b0fc5f2db812846f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6741ba59098e082033ba",
+    "url": "/static/css/main.962db940.chunk.css"
+  },
+  {
+    "revision": "9f056d04b2d355f23056",
+    "url": "/static/js/2.535be06c.chunk.js"
+  },
+  {
+    "revision": "6741ba59098e082033ba",
+    "url": "/static/js/main.75d4121b.chunk.js"
+  },
+  {
+    "revision": "d2a42e71ac49bebe69b9",
+    "url": "/static/js/runtime-main.d40cca16.js"
+  },
+  {
+    "revision": "5b9434401451e921caeb3fe51debf170",
+    "url": "/static/media/carrot.5b943440.png"
+  },
+  {
+    "revision": "3d0474e356915af0beee1b646b79abab",
+    "url": "/static/media/dirt_with_worm.3d0474e3.png"
+  },
+  {
+    "revision": "ab73577a92524ab88deeabc3fbffdfcb",
+    "url": "/static/media/grass.ab73577a.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "2e085af0cc7c4f9be6ebb3e683c36720",
+    "url": "/static/media/potatoes.2e085af0.png"
+  },
+  {
+    "revision": "9ed3375b6bcf79b849222dcff18df50c",
+    "url": "/static/media/pumpkin.9ed3375b.png"
+  },
+  {
+    "revision": "c919ad193543dfc74005acf2189a3053",
+    "url": "/static/media/radish.c919ad19.png"
+  },
+  {
+    "revision": "164cc2fabbcd923be6acf3b5e3ecc647",
+    "url": "/static/media/tomato.164cc2fa.png"
+  },
+  {
+    "revision": "ec9755662755cf7a4759fcf769a7e376",
+    "url": "/static/media/wheat.ec975566.png"
+  }
+]);
