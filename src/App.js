33ed79fbb30dbac3ce715a1a1a52e15071c0b975
Brenda Wallace <brenda.wallace@dia.govt.nz>
2019-12-01 16:54:02 +1300

    Draws the garden

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Garden extends React.Component {
+  state = {
+    rows: [1, 2, 3],
+  };
+
+  render() {
+    return (
+        <div class="game-area">
+        {this.state.rows.map(function(row, i){
+            return <GardenRow key={i} row={row}/>;
+        })}
+        </div>
+      );
+  }
+}
+
+class GardenRow extends React.Component {
+  state = {
+    squares: [1, 2, 3],
+  };
+
+  render() {
+    return (
+        <div class="garden-row">
+          {this.state.squares.map(function(square, i){
+              return <GardenSquare key={i} square={square}/>;
+          })}
+        </div>
+      );
+  }
+}
+
+
+class GardenSquare extends React.Component {
+  render() {
+    return (
+      <div class="square grass"></div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <h1>farm</h1>
+          <Garden />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
